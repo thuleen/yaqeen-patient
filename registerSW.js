2099b1d0b38c83959c4a1614d26a283de8c6d7e1
Azlan Abdul Aziz <azlan@Azlans-MacBook-Air-M1.local>
2022-07-18 10:48:23 +0800

    Added env, version info, update button and implemented sign out action

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/yaqeen-patient/sw.js', { scope: '/yaqeen-patient/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
